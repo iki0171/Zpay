b21804a668fedf941d93dc7217c5b84e53327076
iki0171 <80203189+iki0171@users.noreply.github.com>
2021-08-12 17:36:06 +0900

    Add files via upload

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,3 +1,7 @@
+$(window).ready(function(){
+    ui.localSelect();
+});
+
 const ui = {};
 
 ui.zIndex = 2000;
@@ -18,4 +22,29 @@ ui.bodyScroll = (state,datasetName) => {
         case 'on': return $('body').attr(`data-${datasetName}`,true);
         case 'off': return $('body').attr(`data-${datasetName}`,false);
     }
+}
+
+ui.localSelect = () => {
+
+    $('.step .select,.step .txt_inp').on('click', e=>{
+        e.preventDefault();
+        $('.step > li').removeClass('on');
+
+        if ( e.currentTarget.className != 'select' ) return;
+
+        $(e.currentTarget).parents('li').addClass('on');
+        $('.area_close').addClass('on');
+
+       
+
+        // $('.sch_box').append('<div class="area_close" style="position:fixed; left:0; right:0; top:70px; bottom:0; z-index:9;"></div>')
+    });
+
+    $(document).on('click', '.area_close', e=>{
+        e.preventDefault();
+
+        $('.step > li').removeClass('on');
+        
+        $('.area_close').removeClass('on');
+    });
 }
